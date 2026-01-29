88f24e7f7165895b9b950ad0bb2ad4960fabc7c4
Annika Garg <annikaajaygarg@gmail.com>
2026-01-29 20:47:42 +0800

    Echo user input and exit on bye

--- src/main/java/Shiro.java
+++ src/main/java/Shiro.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
 public class Shiro {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Shiro");
-        System.out.println("What can I do for you?");
+    private static final String line = "    ____________________________________________________________";
+
+    private static void printGreeting() {
+        System.out.println(line);
+        System.out.println("     Hello! I'm Shiro");
+        System.out.println("     What can I do for you?");
+        System.out.println(line);
         System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void printBye() {
+        System.out.println(line);
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println(line);
+        System.out.println();
+    }
+
+    private static void displayEcho(String input) {
+        System.out.println(line);
+        System.out.println("     " + input);
+        System.out.println(line);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        printGreeting();
+        while (true) {
+            System.out.print("> ");
+            String input = in.nextLine();
+            if (input.trim().equals("bye")) {
+                printBye();
+                break;
+            }
+            displayEcho(input);
+        }
     }
 }
