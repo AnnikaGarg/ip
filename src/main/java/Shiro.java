ba3b324cb941123368cd2c278eb5676d07d8116d
Annika Garg <annikaajaygarg@gmail.com>
2026-02-12 21:40:59 +0800

    Update mark/umark message format to reflect task type

--- src/main/java/Shiro.java
+++ src/main/java/Shiro.java
@@ -52,7 +52,7 @@ public class Shiro {
     private static void markMessage(Task task) {
         System.out.println(LINE);
         System.out.println("     Nice! I've marked this task as done:");
-        System.out.println("       [" + task.getStatusIcon() + "] " + task.getDescription());
+        System.out.println("       " + task);
         System.out.println(LINE);
         System.out.println();
     }
@@ -60,7 +60,7 @@ public class Shiro {
     private static void unmarkMessage(Task task) {
         System.out.println(LINE);
         System.out.println("     OK, I've marked this task as not done yet:");
-        System.out.println("       [" + task.getStatusIcon() + "] " + task.getDescription());
+        System.out.println("       " + task);
         System.out.println(LINE);
         System.out.println();
     }
